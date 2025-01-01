6bed2497a144f67cde2c144586dc73a3a61e672e
Yumna Mubeen <yumnamubeen0019@gamil.com>
2025-01-01 22:12:31 +0500

    starting from scratch

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './index.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Settings from './components/Settings';
-import Questions from './components/Questions';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div id="App">
-      <Router>
+      {/* <Router>
         <Routes>
-          <Route path="/" element={<Settings/>}></Route>
-          <Route path="/question" element={<Questions/>}></Route>
+          <Route path="/" element={</>}></Route>
+          <Route path="/question" element={</>}></Route>
         </Routes>
-      </Router>
+      </Router> */}
     </div>
   );
 }
